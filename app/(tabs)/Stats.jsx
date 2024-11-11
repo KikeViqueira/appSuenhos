7659944ac672a1f8ec3ed6706ae8bcaf77636a67
KikeViqueira <enrique.viqueira@rai.usc.es>
2024-11-11 23:11:25 +0100

    Empezando pestaña estadísticas

--- app/(tabs)/Stats.jsx
+++ app/(tabs)/Stats.jsx
@@ -1,13 +1,66 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LineChart } from "react-native-chart-kit";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Estadisticas = () => {
+  //Definimos un estado para saber si el modal de preguntas sobre la calidad del sueño está abierto o cerrado
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <SafeAreaView>
-      <Text>Estadisticas</Text>
-    </SafeAreaView>
-  )
-}
+      <ScrollView>
+        {/* Primera sección de la pestaña de estadísticas
+          que servirá para registrar las horas de sueño del usuario y abrir el modal de preguntas nada más despertarse en relación a su calidad de sueño */}
+        <View>
+          <Text>Registro de Sueño</Text>
+          <TouchableOpacity>
+            <Icon name="moon-o" size={20} color="#111" />
+            <Text>Me voy a dormir</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Icon name="sun-o" size={20} color="#111" />
+            <Text>Me acabo de despertar</Text>
+          </TouchableOpacity>
+        </View>
 
-export default Estadisticas
+        {/* Segunda sección de la pestaña de estadísticas, que hace referencia a la gráfica que recoge las horas que el usario a dormido a lo largo de los días de la semana*/}
+        <View>
+          <LineChart
+            data={{
+              labels: ["L", "M", "M", "J", "V", "S", "D"],
+              datasets: [
+                {
+                  data: [6, 7, 6.5, 8, 7.5, 9, 8.5], // Horas de sueño por día de la semana
+                },
+              ],
+            }}
+            width={Dimensions.get("window").width} // Ancho de la gráfica
+            height={220}
+            bezier // Curva de la línea
+            yAxisLabel="h"
+            yAxisInterval={1} //Intervalo de horas en el eje Y
+            chartConfig={{
+              backgroundGradientFrom: "#1E2923",
+              backgroundGradientTo: "#08130D",
+              //En las dos siguientes propiedades usamos backticks para poder meter el valor de la opacidad median una expresión. Además nos permite escribir en varias líneas si queremos
+              color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // Color de la línea
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`, // Color de las etiquetas
+              strokeWidth: 2, // Grosor de la línea
+              useShadowColorFromDataset: false, // Sombra de la línea
+            }}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Estadisticas;
--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,131 +1,129 @@
-import { View, Text, Image } from 'react-native';
-import { Tabs, Redirect} from 'expo-router';
+import { View, Text, Image } from "react-native";
+import { Tabs, Redirect } from "expo-router";
 
-import icons from '../../constants/icons';
+import icons from "../../constants/icons";
 
-const TabIcon = ({ icon, color, name, focused}) => {
-    return (
-        //Damos estilo a nuestros iconos del tab junto a sus correspondientes nombres
-        <View className='flex items-center justify-center gap-1'>
-            <Image
-                source={icon}
-                resizeMode='contain'
-                tintColor={color}
-                className="w-6 h-6"
-            />
-            <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`} style={{color : color}}>
-                {name}
-            </Text>
-        </View>
-    )
-}
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    //Damos estilo a nuestros iconos del tab junto a sus correspondientes nombres
+    <View className="flex items-center justify-center gap-1">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 
 const Tabslayout = () => {
   return (
     <>
-        <Tabs screenOptions={{
-            //Ocultamos los nombres de los tabs que vienen por defecto
-            tabBarShowLabel: false,
-            //Establecemos los colores de las distintas personalizaciones de los tabs
-            tabBarActiveTintColor: '#6366FF',
-            tabBarInactiveTintColor: '#CDCDE0',
-            //Establecemos ahora los colores de fondo de la caja que agrupa a los tabs
-            tabBarStyle: {
-                backgroundColor: '#323d4f',
-                borderTopCWidth: 1,
-                borderTopColor: '#1e2a47',
-                height: 84,
-            }
+      <Tabs
+        screenOptions={{
+          //Ocultamos los nombres de los tabs que vienen por defecto
+          tabBarShowLabel: false,
+          //Establecemos los colores de las distintas personalizaciones de los tabs
+          tabBarActiveTintColor: "#6366FF",
+          tabBarInactiveTintColor: "#CDCDE0",
+          //Establecemos ahora los colores de fondo de la caja que agrupa a los tabs
+          tabBarStyle: {
+            backgroundColor: "#323d4f",
+            borderTopCWidth: 1,
+            borderTopColor: "#1e2a47",
+            height: 84,
+          },
         }}
-        >
-            <Tabs.Screen
-                name='Stats'
-                options={{
-                    title: 'Estadisticas',
-                    //headerShown: false,
-                    tabBarIcon: ({color, focused}) => (
-                        <TabIcon
-                            icon={icons.barChart}
-                            color={color}
-                            name='Estadísticas'
-                            focused={focused}
-                        />
+      >
+        <Tabs.Screen
+          name="Stats"
+          options={{
+            title: "Estadísticas de Sueño",
+            //headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.barChart}
+                color={color}
+                name="Estadísticas"
+                focused={focused}
+              />
+            ),
+          }}
+        />
 
-                    )
-                }}
-            />
+        <Tabs.Screen
+          name="Alarm"
+          options={{
+            title: "Alarm",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.alarm}
+                color={color}
+                name="Alarm"
+                focused={focused}
+              />
+            ),
+          }}
+        />
 
-            <Tabs.Screen
-                name='Alarm'
-                options={{
-                    title: 'Alarm',
-                    headerShown: false,
-                    tabBarIcon: ({color, focused}) => (
-                        <TabIcon
-                            icon={icons.alarm}
-                            color={color}
-                            name='Alarm'
-                            focused={focused}
-                        />
+        <Tabs.Screen
+          name="Chat"
+          options={{
+            title: "Chat",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.chat}
+                color={color}
+                name="Chat"
+                focused={focused}
+              />
+            ),
+          }}
+        />
 
-                    )
-                }}
-            />
+        <Tabs.Screen
+          name="Tips"
+          options={{
+            title: "Tips",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.tips}
+                color={color}
+                name="Tips"
+                focused={focused}
+              />
+            ),
+          }}
+        />
 
-            <Tabs.Screen
-                name='Chat'
-                options={{
-                    title: 'Chat',
-                    headerShown: false,
-                    tabBarIcon: ({color, focused}) => (
-                        <TabIcon
-                            icon={icons.chat}
-                            color={color}
-                            name='Chat'
-                            focused={focused}
-                        />
-
-                    )
-                }}
-            />
-
-            <Tabs.Screen
-                name='Tips'
-                options={{
-                    title: 'Tips',
-                    headerShown: false,
-                    tabBarIcon: ({color, focused}) => (
-                        <TabIcon
-                            icon={icons.tips}
-                            color={color}
-                            name='Tips'
-                            focused={focused}
-                        />
-
-                    )
-                }}
-            />
-
-            <Tabs.Screen
-                name='Profile'
-                options={{
-                    title: 'Profile',
-                    headerShown: false,
-                    tabBarIcon: ({color, focused}) => (
-                        <TabIcon
-                            icon={icons.profile}
-                            color={color}
-                            name='Profile'
-                            focused={focused}
-                        />
-
-                    )
-                }}
-            />
-
-        </Tabs>
+        <Tabs.Screen
+          name="Profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.profile}
+                color={color}
+                name="Profile"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
     </>
-  )
-}
+  );
+};
 
-export default Tabslayout
+export default Tabslayout;
