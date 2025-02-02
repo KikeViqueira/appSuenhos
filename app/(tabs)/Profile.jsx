9f1a809793c449aece2acd87f900dbc9490ac23a
KikeViqueira <enrique.viqueira@rai.usc.es>
2025-02-02 22:28:33 +0100

    Cambio de foto en la sección de perfil mediante distintas opciones

--- app/(tabs)/Profile.jsx
+++ app/(tabs)/Profile.jsx
@@ -1,9 +1,93 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import { View, Text, Image, TouchableOpacity, Alert } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Camera, Edit2 } from "lucide-react-native";
+import placeholderImage from "../../assets/images/placeholder.png";
+import * as ImagePicker from "expo-image-picker";
+import PictureOptions from "../../components/PictureOptions";
 
 const Profile = () => {
+  //Hacemos states tanto para guardar la foto como para controlar que el modal de opciones de cámara este desplegado o no
+  const [imagen, setImage] = useState(placeholderImage);
+  const [showModal, setshowModal] = useState(false);
+
+  //hacemos función de guardar foto que incluye las opciones de sacar foto o elegir una de la galería, en base al parámetro que reciba
+  const uploadPicture = async ({ mode }) => {
+    //tenemos que definir la función como asíncrona para poder usar métodos await que esperan a que se resuelvan promesas
+
+    let result = {}; //Variable para guardar la foto del user de manera local
+
+    if (mode === "gallery") {
+      try {
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+        result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ["images"], // El contenido que dejamos seleccionar de la galería son las fotos
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+      } catch (error) {
+        //Notificamos al usuario de que ha sucedido un error a la hora de seleccionar una determinada foto de la galería
+        Alert.alert(
+          "Error",
+          "Ha sucedido un error a la hora de seleccionar la foto de la galería, inténtalo de nuevo"
+        );
+      }
+    } else {
+      //Estamos en el caso de que el user sube una foto de perfil sacada directamente desde la cámara
+      try {
+        await ImagePicker.requestCameraPermissionsAsync();
+        result = await ImagePicker.launchCameraAsync({
+          cameraType: ImagePicker.CameraType.front, //hacemos que la cámara por defecto sea la frontal a la hora de sacar la foto
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+      } catch (error) {
+        //Notificamos al usuario de que ha sucedido un error a la hora de sacar la foto
+        Alert.alert(
+          "Error",
+          "Ha sucedido un error a la hora de sacar la foto, inténtalo de nuevo"
+        );
+      }
+    }
+
+    //Comprobamos que el resultado no haya sido cancelado y guardamos la foto
+    if (!result.canceled) {
+      savePicture({ imagen: result.assets[0].uri });
+    }
+  };
+
+  //Función para borrar la foto que el user tiene de perfil
+  const deletePicture = async () => {
+    try {
+      setImage(placeholderImage);
+      setshowModal(false);
+    } catch (error) {
+      Alert.alert(
+        "Error",
+        "Ha sucedido un error a la hora de borrar la foto, inténtalo de nuevo"
+      );
+    }
+  };
+
+  //Función para guardar la foto en el estado que hemos definido y a mayores guardarla en la base de datos
+  const savePicture = async ({ imagen }) => {
+    // TODO : Implementar el guardado en la base de datos cuando tengamos la API
+    try {
+      console.log("Guardando la foto", imagen);
+      //Como estamos guardando una foto dinámica, tenemos que pasarla a un URI y asi pasar el objeto y que react sepa que es una foto que no está en el proyecto
+      //Y de esta manera puede cargarla como imagen en la aplicación
+      setImage({ uri: imagen });
+      setshowModal(false);
+    } catch (error) {
+      Alert.alert(
+        "Error",
+        "Ha sucedido un error a la hora de guardar la foto, inténtalo de nuevo"
+      );
+    }
+  };
+
   return (
     <SafeAreaView className="flex-1 bg-primary">
       <View className="flex-1 px-4 py-6">
@@ -18,15 +102,11 @@ const Profile = () => {
         {/* User Profile Photo */}
         <View className="items-center mb-8">
           <View className="relative">
-            <Image
-              source={{
-                uri: "https://ui-avatars.com/api/?name=Enrique+Viqueira&background=6366FF&color=fff",
-              }}
-              className="w-32 h-32 rounded-full"
-            />
+            <Image source={imagen} className="w-32 h-32 rounded-full" />
             <TouchableOpacity
               className="absolute bottom-0 right-0 bg-[#6366FF] p-2 rounded-full"
-              // onPress will be implemented later
+              // Cuando hagamos click en el botón de la cámara tenemos que enseñar el modal con las diferentes opciones al usuario
+              onPress={() => setshowModal(true)}
             >
               <Camera size={20} color="white" />
             </TouchableOpacity>
@@ -35,6 +115,14 @@ const Profile = () => {
             Enrique Viqueira
           </Text>
         </View>
+
+        {/* Picture Options Modal */}
+        <PictureOptions
+          visible={showModal}
+          setModalVisible={setshowModal}
+          uploadPicture={uploadPicture}
+          deletePicture={deletePicture}
+        />
 
         {/* Personal Information */}
         <View className="bg-[#1e2a47] rounded-xl p-4 mb-6">
---
+++ components/PictureOptions.jsx
@@ -0,0 +1,75 @@
+import { View, Text, Modal, TouchableOpacity } from "react-native";
+import React from "react";
+import { Camera, Image, Trash2, X } from "lucide-react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const PictureOptions = ({
+  visible,
+  setModalVisible,
+  uploadPicture,
+  deletePicture,
+}) => {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={visible}
+      onRequestClose={() => setModalVisible(false)}
+    >
+      <TouchableOpacity
+        className="flex-1 justify-center items-center bg-black/50"
+        activeOpacity={1}
+      >
+        <View className="bg-[#1e2a47] p-6 rounded-xl w-[80%] max-w-[300px]">
+          <View className="flex-row justify-between items-center mb-4">
+            <Text className="text-lg font-bold color-white">
+              Opciones de Foto
+            </Text>
+            <TouchableOpacity onPress={() => setModalVisible(false)}>
+              <X size={24} color="#fff" />
+            </TouchableOpacity>
+          </View>
+
+          <View className="gap-6">
+            {/*Botón que representa sacar una foto con la cámara y ponerla de perfil */}
+            <TouchableOpacity
+              className="flex-row items-center gap-2 bg-[#6366ff] p-3 rounded-xl"
+              onPress={() => {
+                uploadPicture({ mode: "photo" });
+              }}
+            >
+              <Camera size={24} color="white" className="mr-3" />
+              <Text className="text-white font-psemibold">Tomar Foto</Text>
+            </TouchableOpacity>
+
+            {/*Botón que representa poner una foto de la galería como de perfil */}
+            <TouchableOpacity
+              className="flex-row items-center gap-2 bg-[#6366ff] p-3 rounded-xl"
+              onPress={() => {
+                uploadPicture({ mode: "gallery" });
+              }}
+            >
+              <Image size={24} color="white" className="mr-3" />
+              <Text className="text-white font-psemibold">
+                Elegir de Galería
+              </Text>
+            </TouchableOpacity>
+
+            {/*Botón que representa eliminar la foto de perfil actual*/}
+            <TouchableOpacity
+              className="flex-row items-center gap-2 bg-[#ff6b6b] p-3 rounded-xl"
+              onPress={() => {
+                deletePicture();
+              }}
+            >
+              <Trash2 size={24} color="white" className="mr-3" />
+              <Text className="text-white font-psemibold">Eliminar Foto</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </Modal>
+  );
+};
+
+export default PictureOptions;
